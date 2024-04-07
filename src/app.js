e6fafa3bed98f3f1c4cd05639d118b38ca9d588b
Evert Ortiz <evert.ortiz.m@gmail.com>
2024-04-07 15:50:45 -0500

    refactor: integrating api test and nodemon

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
+  const { array } = req.body;
   res.status(200).send("Hello World!");
 });
 
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,13 +1,14 @@
 const request = require("supertest");
 const app = require("../src/app");
-
-describe("Test the root path", () => {
-  test("It should response the GET method", done => {
-    request(app)
-      .get("/")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
+const entry1 = [
+  [1, 1, 1],
+  [1, 1, 1],
+  [1, 1, 1]
+];
+describe("Test", () => {
+  test("It should response the POST method", async () => {
+    const response = await request(app).post("/").send({ array: entry1 });
+    // console.log(response);
+    expect(response.statusCode).toBe(200);
   });
 });
